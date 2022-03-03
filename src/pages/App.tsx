ac927971ab0f8a8b482d63116ad5d9b185974124
BrunoMatosSilva <brunomatos2010@outlook.com>
2022-03-03 15:28:20 -0300

    Theme styled

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,53 +1,16 @@
-import '../styles/App.css';
-
-import React, { useState } from 'react';
-
-import logo from '../assets/logo.svg';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
+import { GlobalStyles } from '../styles/global';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <h1>Hello World!</h1>
+      </ThemeProvider>
+    </>
   );
 }
 
